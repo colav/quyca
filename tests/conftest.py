b231d77d223c73572b3c8ae58a2c2903f141922c
joselomanuelo <joselomanuelo@gmail.com>
2024-11-20 08:13:29 -0500

    test: use fixtures for random affiliations hashes

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,6 @@
 from pytest import fixture
+
+from infrastructure.mongo import database
 from quyca.app import create_app
 
 
@@ -10,12 +12,46 @@ def app():
             "TESTING": True,
         }
     )
-
     yield app
 
 
 @fixture()
 def client(app):
     client = app.test_client()
-
     yield client
+
+
+@fixture()
+def random_institution_id():
+    return (
+        database["affiliations"]
+        .aggregate([{"$match": {"types.type": "education"}}, {"$sample": {"size": 1}}])
+        .next()["hash"]
+    )
+
+
+@fixture()
+def random_faculty_id():
+    return (
+        database["affiliations"]
+        .aggregate([{"$match": {"types.type": "faculty"}}, {"$sample": {"size": 1}}])
+        .next()["hash"]
+    )
+
+
+@fixture()
+def random_department_id():
+    return (
+        database["affiliations"]
+        .aggregate([{"$match": {"types.type": "department"}}, {"$sample": {"size": 1}}])
+        .next()["hash"]
+    )
+
+
+@fixture()
+def random_group_id():
+    return (
+        database["affiliations"]
+        .aggregate([{"$match": {"types.type": "group"}}, {"$sample": {"size": 1}}])
+        .next()["hash"]
+    )
--- tests/feature/affiliations/plot/plot_citations_by_faculty_test.py
+++ tests/feature/affiliations/plot/plot_citations_by_faculty_test.py
@@ -1,12 +1,4 @@
-from quyca.infrastructure.mongo import database
-
-
-def test_it_can_plot_citations_by_faculty_by_institution(client):
-    random_institution_id = (
-        database["affiliations"]
-        .aggregate([{"$match": {"types.type": "education"}}, {"$sample": {"size": 1}}])
-        .next()["_id"]
-    )
+def test_it_can_plot_citations_by_faculty_by_institution(client, random_institution_id):
     response = client.get(
         f"/app/affiliation/institution/{random_institution_id}/research/products?plot=citations_by_faculty"
     )
